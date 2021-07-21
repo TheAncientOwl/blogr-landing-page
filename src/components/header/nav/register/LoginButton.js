06ee77bed77c481ebed2f39d385320b7b82ad090
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-21 16:46:02 +0300

    added underline on hover.

--- src/components/header/nav/register/LoginButton.js
+++ src/components/header/nav/register/LoginButton.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin-right: 1em;
+  cursor: pointer;
+
+  :hover {
+    text-decoration: underline;
+  }
 `;
 
 export default function LoginButton() {
